1bf3a3bf5970fd28a50f489e8e5e68cb9c6b782c
Daniel <allinasdaniel@gmail.com>
2024-11-09 19:02:01 +0800

    starting on the ground entity

--- game.py
+++ game.py
@@ -12,6 +12,7 @@ pygame.display.set_icon(pygame.image.load('assets/bird1.png'))
 clock = pygame.time.Clock()
 
 bg = background.Background(0, 0, 1.2)
+grd = ground.Ground(0, (config.SCREENHEIGTH - (config.SCREENHEIGTH // 6)), 1.2)
 
 run = True
 
@@ -22,8 +23,13 @@ while run:
             run = False
 
     screen.fill('black')
+
     bg.draw(screen)
     bg.update()
+
+    grd.draw(screen)
+    grd.update()
+
     clock.tick(config.FPS)
     pygame.display.flip()
 
---
+++ objects/ground.py
@@ -0,0 +1,26 @@
+import pygame
+from src import config
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.img = pygame.image.load('assets/ground.png').convert_alpha()
+        self.img = pygame.transform.scale_by(self.img, scale)
+        self.img_rect = self.img.get_rect(topleft = (x,y))
+        self.img_rect2 = self.img.get_rect(topleft = (self.img.get_width(), y))
+    
+    
+    def draw(self, screen):
+        screen.blit(self.img, self.img_rect)
+        screen.blit(self.img, self.img_rect2)
+
+    def update(self):
+        self.img_rect.x -= config.GROUND_SPD
+        self.img_rect2.x -= config.GROUND_SPD
+
+        if self.img_rect.right <= 0:
+            self.img_rect.x = self.img_rect2.right 
+
+        if self.img_rect2.right <= 0:
+            self.img_rect2.x = self.img_rect.right    
