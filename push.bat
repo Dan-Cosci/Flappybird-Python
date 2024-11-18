b10a101f0051c02f7d380560e76331a4462dc690
Daniel <allinasdaniel@gmail.com>
2024-11-18 13:10:02 +0800

    fixing merges

--- push.bat
+++
@@ -1,5 +0,0 @@
-git status
-git add *
-git status
-git commit -a -m %1 
-git push origin main
--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python main.py
-
-pause
-cls
-echo program finished running
-set workdir="C:\Users\LENOVO\code_vscode\Flappybird"
-dir s/w/o/p %workdir%
